01158e6a62106df0bfa900d0533a929cf215990b
JaeSeoHan <hanahyun1@gmail.com>
2025-06-16 18:43:39 +0900

    feat:Create Member domain class
    - summary:
      create Member class
    
    - changes:
      1. success to connect with AWS RDS
      2. create user 'damultalk' to control db
      3. create Member class

---
+++ backend/src/main/java/com/demo/damulTalk/member/domain/Member.java
@@ -0,0 +1,21 @@
+package com.demo.damulTalk.member.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    private Long memberId;
+    private String username;
+    private String password;
+    private String nickname;
+    private String profileImageUrl;
+    private String statusMessage;
+    private LocalDateTime joinedAt;
+
+}
