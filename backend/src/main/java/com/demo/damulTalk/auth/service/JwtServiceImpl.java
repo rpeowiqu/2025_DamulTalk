8eb7f04786b23b9f2d83e062763c57b6e14ecb15
JaeSeoHan <hanahyun1@gmail.com>
2025-06-17 16:25:31 +0900

    feat: Implement JwtVerificationFilter
    - summary:
      implement JwtVerification Filter
    
    - changes:
      1. add token handling methods
      2. server can controller requests by token's status

--- backend/src/main/java/com/demo/damulTalk/auth/service/JwtServiceImpl.java
+++ backend/src/main/java/com/demo/damulTalk/auth/service/JwtServiceImpl.java
@@ -7,9 +7,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
@@ -18,6 +20,8 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
+@Service
+@RequiredArgsConstructor
 public class JwtServiceImpl implements JwtService {
 
     @Value("${jwt.secret}")
--- backend/src/main/java/com/demo/damulTalk/filter/JwtVerificationFilter.java
+++ backend/src/main/java/com/demo/damulTalk/filter/JwtVerificationFilter.java
@@ -1,15 +1,124 @@
 package com.demo.damulTalk.filter;
 
+import com.demo.damulTalk.auth.service.JwtService;
+import com.demo.damulTalk.member.domain.Member;
+import com.demo.damulTalk.member.service.CustomUserDetailsService;
+import com.demo.damulTalk.util.CookieUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class JwtVerificationFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String REFRESH_TOKEN = "refresh_token";
 
+    private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
+    private final CookieUtil cookieUtil;
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+    private final List<String> excludedUrls = Arrays.asList(
+            "/api/v1/auth/**"
+    );
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("[JwtVerificationFilter] filter chain 시작");
+
+        String authorizationHeader = request.getHeader("Authorization");
+        String accessToken = null;
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            accessToken = authorizationHeader.substring(7);
+        }
+
+        Cookie refreshCookie = cookieUtil.getCookie(request, REFRESH_TOKEN);
+        String refreshToken = (refreshCookie != null) ? refreshCookie.getValue() : null;
+
+        try {
+            if (accessToken != null && refreshToken != null) {
+                try {
+                    processAccessToken(accessToken, response);
+                    filterChain.doFilter(request, response);
+                    return;
+                } catch(ExpiredJwtException e) {
+                    log.info("[JwtVerificationFilter] Access Token 만료, Refresh Token으로 갱신 시도");
+                    handleExpiredToken(response, refreshToken);
+                    return;
+                }
+            } else if(refreshToken != null) {
+                handleExpiredToken(response, refreshToken);
+                return;
+            }
+        } catch(JwtException e) {
+            log.error("[JwtVerificationFilter] Jwt 처리 중 오류 발생 - {}", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        log.info("[JwtVerificationFilter] JWT 검증 완료");
+        filterChain.doFilter(request, response);
+    }
+
+    private void processAccessToken(String accessToken, HttpServletResponse response) {
+        String username = jwtService.getUsernameFromToken(accessToken);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtService.isValid(accessToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+    }
+
+    private void handleExpiredToken(HttpServletResponse response, String refreshToken) {
+        if(refreshToken != null) {
+            Member user = ((CustomUserDetails) userDetailsService.loadUserByUsername(
+                    jwtService.extractUsername(refreshToken))).getMember();
+
+            Map<String, String> tokens = jwtService.rotateTokens(refreshToken, user);
+            if(tokens != null) {
+                cookieUtil.addCookie(response, REFRESH_TOKEN, tokens.get("refreshToken"), (int) (jwtService.getRefreshTokenExpire() / 1000));
+
+                response.setHeader("Authorization",  "Bearer " + tokens.get("accessToken"));
+
+                processAccessToken(tokens.get("accessToken"), response);
+                return;
+            }
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        boolean shouldNotFilter = excludedUrls.stream()
+                .anyMatch(pattern -> pathMatcher.match(pattern, path));
+        log.info("[JwtVerificationFilter] JWT 필터 제외 여부 확 - URI: {}, 제외: {}", path, shouldNotFilter);
+        return shouldNotFilter;
     }
 
 }
