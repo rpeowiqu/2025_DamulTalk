b484584162810b883f83516cba337d0ab77fb7ab
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-06-26 23:50:12 +0900

    [FE] design: Modify shadcn dialog header responsive text align

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -78,7 +78,7 @@ const DialogHeader = ({ className, ...props }: React.ComponentProps<"div">) => {
   return (
     <div
       data-slot="dialog-header"
-      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      className={cn("flex flex-col gap-2 text-left", className)}
       {...props}
     />
   );
