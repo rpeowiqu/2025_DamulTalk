2a37a336e66f1e43c83d915368339dca10a2e795
JaeSeoHan <hanahyun1@gmail.com>
2025-06-26 17:21:34 +0900

    feat: Setup WebSocket configuration and redis subscriber for user status
    - summary:
      configure websocket and setup redis subscriber for user status
    
    - changes:
      1. add WebSocketConfig
      2. add UserStatusSubscriber

--- backend/src/main/java/com/demo/damulTalk/config/RedisConfig.java
+++ backend/src/main/java/com/demo/damulTalk/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.demo.damulTalk.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
     @Value("${spring.data.redis.host:localhost}")
