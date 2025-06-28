ef0eed87e2e3e425d4ae204c08fc49192e662fc7
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-06-28 19:18:48 +0900

    [FE] feat: Add infinite scroll custom hook

---
+++ frontend/src/hooks/common/use-infinite-scroll.ts
@@ -0,0 +1,59 @@
+import {
+  useInfiniteQuery,
+  type InfiniteData,
+  type UseInfiniteQueryOptions,
+} from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
+
+interface UseInfiniteScrollOptions<T>
+  extends UseInfiniteQueryOptions<T, Error, InfiniteData<T>> {
+  rootMargin?: string;
+  threshold?: number;
+}
+
+const useInfiniteScroll = <T>({
+  rootMargin,
+  threshold,
+  ...props
+}: UseInfiniteScrollOptions<T>) => {
+  const targetRef = useRef<HTMLDivElement>(null);
+  const response = useInfiniteQuery({
+    ...props,
+  });
+
+  useEffect(() => {
+    if (!targetRef.current) {
+      return;
+    }
+
+    const handleIntersect = ([entry]: IntersectionObserverEntry[]) => {
+      if (
+        entry.isIntersecting &&
+        response.hasNextPage &&
+        !response.isFetchingNextPage
+      ) {
+        response.fetchNextPage();
+      }
+    };
+    const options = {
+      rootMargin,
+      threshold,
+    };
+    const observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(targetRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [
+    rootMargin,
+    threshold,
+    response.hasNextPage,
+    response.isFetchingNextPage,
+    response.fetchNextPage,
+  ]);
+
+  return { targetRef, ...response };
+};
+
+export default useInfiniteScroll;
