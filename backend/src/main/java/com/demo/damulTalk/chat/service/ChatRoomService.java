b59ef8661012dbd67f3bd456c928e96adf9b238c
JaeSeoHan <hanahyun1@gmail.com>
2025-07-03 21:30:29 +0900

    feat: Add Controller, Service, Dto and Entity under chat domain
    - summary:
      Add ChatController, ChatService, ChatMessage, ChatRoom entities, ChatRoomInfo dto under chat domain
    
    - changes:
      1. Add ChatController has getChatRooms endpoints
      2. Add ChatService has a getChatRooms method
      3. Add ChatMessage and ChatRoom entities
      4. Add ChatRoomInfo dto for api to get chat rooms

---
+++ backend/src/main/java/com/demo/damulTalk/chat/service/ChatRoomService.java
@@ -0,0 +1,11 @@
+package com.demo.damulTalk.chat.service;
+
+import com.demo.damulTalk.chat.dto.ChatRoomInfo;
+
+import java.util.List;
+
+public interface ChatRoomService {
+
+    List<ChatRoomInfo> getChatRooms();
+
+}
