ab5a0384795673c9f6681fdeadd09a28ee460ea0
JongWoo <jinlaove17@naver.com>
2025-07-04 16:35:36 +0900

    [FE] feat: Add friend request acception

--- frontend/src/hooks/community/use-accept-friend-request.ts
+++ frontend/src/hooks/community/use-accept-friend-request.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { postAcceptFriendRequest } from "@/services/community/api";
-import type { FriendRequestsResponse } from "@/types/community/type";
+import type { FriendRequestsResponse, User } from "@/types/community/type";
 
 const useAcceptFriendRequest = (userId: number) => {
   const queryClient = useQueryClient();
@@ -10,12 +10,20 @@ const useAcceptFriendRequest = (userId: number) => {
     mutationKey: ["accept-friend-request", userId],
     mutationFn: (userId: number) => postAcceptFriendRequest(userId),
     onSuccess: () => {
+      // 이전에 받은 친구 요청 리스트에 대한 데이터에서 현재 친구 요청을 제거한다.
       queryClient.setQueryData<FriendRequestsResponse>(
         ["friend-requests"],
         (prev) => prev?.filter((user) => user.userId !== userId) ?? [],
       );
-      queryClient.invalidateQueries({
-        queryKey: ["friend-requests"],
+      // // 이 데이터는 더이상 유효하지 않기 때문에 새로운 친구 요청이 올 때 리패칭 하도록 무효화 해주어야 한다.
+      // queryClient.invalidateQueries({
+      //   queryKey: ["friend-requests"],
+      // });
+
+      // 친구 목록을 다시 불러온다.
+      const currentUser = queryClient.getQueryData<User>(["current-user"]);
+      queryClient.refetchQueries({
+        queryKey: ["friends", currentUser?.userId],
       });
     },
   });
