54e7dc99c86c29b5665309b42ff9cb80e61e60ae
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-07-06 22:36:12 +0900

    [FE] feat: Subscribe notification

--- frontend/src/components/side-bar/side-bar.tsx
+++ frontend/src/components/side-bar/side-bar.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import SideBarTab from "@/components/side-bar/side-bar-tab";
 import SideBarContent from "@/components/side-bar/side-bar-content";
 import { type SideBarTabType } from "@/types/side-bar/type";
+import { WebSocketContext } from "@/contexts/chat/web-socket-provider";
+import useCurrentUser from "@/hooks/auth/use-current-user";
 
 const SideBar = () => {
   const [currentTab, setCurrentTab] = useState<SideBarTabType>("FRIEND");
+  const { data } = useCurrentUser();
+  const client = useContext(WebSocketContext);
+
+  useEffect(() => {
+    if (!data || !client) {
+      return;
+    }
+
+    const subscription = client.subscribe(
+      `/sub/notifications/${data.userId}`,
+      (message) => {
+        console.log("Message received:", message.body);
+      },
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [data, client]);
 
   return (
     <aside className="sticky top-0 flex h-dvh border-r border-neutral-200">
