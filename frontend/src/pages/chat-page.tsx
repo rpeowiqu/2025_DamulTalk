4286200ebe46375206165d5ae243c27c967ff93d
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-07-06 13:56:52 +0900

    [FE] feat: Add chat room header, content skeleton ui
    - Add chat room info fetching api

--- frontend/src/pages/chat-page.tsx
+++ frontend/src/pages/chat-page.tsx
@@ -1,14 +1,30 @@
+import { useParams } from "react-router-dom";
+
 import ChatRoomContent from "@/components/chat/chat-room-content";
 import ChatRoomHeader from "@/components/chat/chat-room-header";
 import FileUploadProvider from "@/contexts/chat/file-upload-provider";
-import ChatRoomDummyData from "@/mocks/chat-room.json";
+import useChatRoom from "@/hooks/chat/use-chat-room";
+import ChatRoomHeaderSkeleton from "@/components/chat/chat-room-header-skeleton";
+import ChatRoomContentSkeleton from "@/components/chat/chat-room-content-skeleton";
 
 const ChatPage = () => {
+  const { roomId } = useParams();
+  const { data, isLoading } = useChatRoom(roomId ? Number(roomId) : 0);
+
   return (
     <FileUploadProvider>
       <div className="flex h-full flex-col bg-neutral-50">
-        <ChatRoomHeader room={ChatRoomDummyData} />
-        <ChatRoomContent />
+        {isLoading ? (
+          <>
+            <ChatRoomHeaderSkeleton />
+            <ChatRoomContentSkeleton />
+          </>
+        ) : data ? (
+          <>
+            <ChatRoomHeader room={data} />
+            <ChatRoomContent />
+          </>
+        ) : null}
       </div>
     </FileUploadProvider>
   );
