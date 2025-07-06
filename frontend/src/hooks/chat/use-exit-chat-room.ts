d12192c0158e59d98f833a0543b173fc3f2515d0
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-07-06 17:03:49 +0900

    [FE] feat: Add chat room exit api
    - Add custom hook related chat room exit

---
+++ frontend/src/hooks/chat/use-exit-chat-room.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+import { deleteExitChatRoom } from "@/services/chat/api";
+import type { ChatRoomPreviewsResponse } from "@/types/chat/type";
+import useCurrentUser from "@/hooks/auth/use-current-user";
+
+const useExitChatRoom = (roomId: number) => {
+  const queryClient = useQueryClient();
+  const { data } = useCurrentUser();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationKey: ["exit-chat-room", roomId],
+    mutationFn: (roomId: number) => deleteExitChatRoom(roomId),
+    onSuccess: (_, roomId) => {
+      // 사이드 바의 채팅방 목록에서 방금 나간 채팅방을 제거하도록 캐시에 저장된 상태를 변경
+      queryClient.setQueryData<ChatRoomPreviewsResponse>(
+        ["chat-room-previews"],
+        (prev) => prev?.filter((item) => item.roomId !== roomId) ?? [],
+      );
+      toast.success("해당 채팅방에서 빠져 나왔어요");
+
+      // 유저의 프로필 페이지로 리다이렉트
+      navigate(`/profiles/${data ? data.userId : 0}`);
+    },
+  });
+};
+
+export default useExitChatRoom;
