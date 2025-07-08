9aaba0389df69c75b69ef7c5b1dbc949f15b85e3
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-07-08 21:21:58 +0900

    [FE] feat: Add chat message api

--- frontend/src/components/chat/chat-message-list.tsx
+++ frontend/src/components/chat/chat-message-list.tsx
@@ -1,8 +1,9 @@
-import type { Ref } from "react";
+import { useRef, type Ref } from "react";
 
 import ChatMessage from "@/components/chat/chat-message";
 import type { Message } from "@/types/chat/type";
 import { cn } from "@/utils/style";
+import useChatMessages from "@/hooks/chat/use-chat-messages";
 
 interface ChatMessageListProps {
   chatMessages: Message[];
@@ -17,12 +18,22 @@ const ChatMessageList = ({
   className,
   onSelect,
 }: ChatMessageListProps) => {
+  const { targetRef, data } = useChatMessages();
+  const lastReadRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className={cn("flex flex-col gap-6", className)}>
+      <div ref={targetRef} className="-mb-6" />
+      {data?.pages.map((page) =>
+        page.data.map((item) => (
+          <ChatMessage key={item.messageId} message={item} />
+        )),
+      )}
+      <div ref={lastReadRef} className="-my-6" />
       {chatMessages.map((item) => (
         <ChatMessage key={item.messageId} message={item} onClick={onSelect} />
       ))}
-      <div ref={bottomRef} />
+      <div ref={bottomRef} className="-mt-6" />
     </div>
   );
 };
---
+++ frontend/src/hooks/chat/use-chat-messages.ts
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+
+import useInfiniteScroll from "@/hooks/common/use-infinite-scroll";
+import { getMessages } from "@/services/chat/api";
+import type { InfiniteScrollType } from "@/types/common/type";
+import type { Message } from "@/types/chat/type";
+
+const useChatMessages = () => {
+  const { roomId } = useParams();
+
+  return useInfiniteScroll({
+    queryKey: ["chat-messages", roomId],
+    queryFn: async ({ pageParam }) => {
+      const response = await getMessages({
+        roomId: Number(roomId),
+        cursor: typeof pageParam === "string" ? pageParam : undefined,
+        size: 50,
+      });
+      if (response.status === 204) {
+        return {
+          data: [],
+          meta: {
+            hasNext: false,
+            nextCursor: "",
+          },
+        };
+      }
+
+      const data = await response.json<InfiniteScrollType<Message>>();
+      return data;
+    },
+    initialPageParam: null,
+    getNextPageParam: (lastPage) =>
+      lastPage.meta.hasNext ? lastPage.meta.nextCursor : null,
+    enabled: !!roomId,
+  });
+};
+
+export default useChatMessages;
