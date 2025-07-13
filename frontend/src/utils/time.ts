87af9d0fd1c8c3e2791064a729adad27a4be9572
JongWoo <jinlaove17@naver.com>
2025-07-13 22:30:51 +0900

    [FE] fix: Modify time format logic

--- frontend/src/utils/time.ts
+++ frontend/src/utils/time.ts
@@ -1,12 +1,7 @@
 export const getFormattedTime = (date: string) => {
   const now = new Date(date);
+  const hours = now.getHours();
   const minutes = now.getMinutes();
 
-  let hours = now.getHours();
-  hours = hours % 12;
-  if (hours === 0) {
-    hours = 12;
-  }
-
-  return `${hours >= 12 ? "오후" : "오전"} ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+  return `${hours >= 12 ? "오후" : "오전"} ${(hours % 12).toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
 };
