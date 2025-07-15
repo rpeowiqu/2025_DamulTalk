f4739d7419b8af32088613a16aaeb67beaa108cd
JongWoo <jinlaove17@naver.com>
2025-07-15 16:27:33 +0900

    [FE] feat: Add temparary redirect setting

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import MainLayout from "@/components/layout/main-layout";
 import ProfilePage from "@/pages/profile-page";
@@ -11,6 +11,12 @@ import PasswordResetForm from "@/components/auth/password-reset-form";
 import ProtectedRoute from "@/components/route/ProtectedRoute";
 
 const router = createBrowserRouter([
+  // 온보딩 페이지를 만들기 전까지 "/"에 접속하면 로그인 페이지로 리다이렉트 하도록 설정
+  {
+    path: "/",
+    element: <Navigate to={"/login"} />,
+  },
+
   {
     element: <ProtectedRoute />,
     children: [
