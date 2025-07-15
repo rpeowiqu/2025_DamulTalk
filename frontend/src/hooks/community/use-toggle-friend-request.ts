e5b54fe7c09d8ce50e4588366566aa403c9cf868
JongWoo <jinlaove17@naver.com>
2025-07-15 12:33:31 +0900

    [FE] fix: Resolve ui update when friend deleted

--- frontend/src/hooks/community/use-toggle-friend-request.ts
+++ frontend/src/hooks/community/use-toggle-friend-request.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import {
   deleteFriendRequest,
@@ -8,13 +8,18 @@ import {
 import type {
   FriendRequestRequest,
   FriendRequestType,
+  FriendsResponse,
 } from "@/types/community/type";
+import useCurrentUser from "@/hooks/auth/use-current-user";
 
 const useToggleFriendRequest = (
   userId: number,
   initState: FriendRequestType,
 ) => {
   const [optimisticState, setOptimisticState] = useState(initState);
+  const { data } = useCurrentUser();
+  const queryClient = useQueryClient();
+
   const requestMutation = useMutation({
     mutationKey: ["request-friend", userId],
     mutationFn: (request: FriendRequestRequest) => postFriendRequest(request),
@@ -31,6 +36,17 @@ const useToggleFriendRequest = (
     mutationFn: (userId: number) => deleteFriendRequest(userId),
     onMutate: () => {
       setOptimisticState("NONE");
+    },
+    onSuccess: (_, targetId) => {
+      // 삭제 요청이 성공했다면, 해당 유저의 프로필을 리패칭 하여 최신화 한다.
+      queryClient.invalidateQueries({ queryKey: ["profile", targetId] });
+      queryClient.invalidateQueries({ queryKey: ["friends", targetId] });
+
+      // 삭제 요청이 성공했다면, 사이드 바에서 해당 친구를 삭제한다.
+      queryClient.setQueryData<FriendsResponse>(
+        ["friends", Number(data?.userId)],
+        (prev) => prev?.filter((item) => item.userId !== targetId) ?? [],
+      );
     },
     onError: () => {
       setOptimisticState(initState);
