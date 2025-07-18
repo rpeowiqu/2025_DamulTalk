47de58b4a983b0f0d6ed784f8fed60f65fe85652
JongWoo <jinlaove17@naver.com>
2025-07-19 00:35:05 +0900

    [FE] chore: Add react query devtool for debugging caching data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import router from "@/routes/router";
 import { Toaster } from "@/components/ui/sonner";
@@ -11,6 +12,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>
       <Toaster position="top-center" richColors />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
