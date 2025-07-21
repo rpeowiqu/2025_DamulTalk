5e052691f4af99f861928f1a564fdaa0b64b8e4e
JaeSeoHan <hanahyun1@gmail.com>
2025-07-21 12:02:22 +0900

    refactor: Add userId when system message is sent for user's exit

---
+++ backend/src/main/java/com/demo/damulTalk/chat/dto/ChatExitSystemMessage.java
@@ -0,0 +1,25 @@
+package com.demo.damulTalk.chat.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatExitSystemMessage extends ChatSystemMessage {
+
+    private Integer userId;
+
+    public static ChatExitSystemMessage of(ChatSystemMessage base, Integer userId) {
+        ChatExitSystemMessage msg = new ChatExitSystemMessage();
+        msg.setMessageId(base.getMessageId());
+        msg.setSenderId(base.getSenderId());
+        msg.setMessageType(base.getMessageType());
+        msg.setContent(base.getContent());
+        msg.setSendTime(base.getSendTime());
+        msg.setUserId(userId);
+        return msg;
+    }
+
+}
