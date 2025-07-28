aa3c7c8cae16a82de297c6d0da4629c9a9a64683
JongWoo <jinlaove17@naver.com>
2025-07-28 23:47:57 +0900

    [FE] feat: Add intro page
    - Add intro image, video files
    - Inactive react query devtool

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import router from "@/routes/router";
 import { Toaster } from "@/components/ui/sonner";
@@ -12,7 +12,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>
       <Toaster position="top-center" richColors />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 };
