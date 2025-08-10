d8b1bafd3af6fd7145bce952f7eca9645d3cc69a
JongWoo <jinlaove17@naver.com>
2025-08-10 19:13:24 +0900

    [FE] feat: Add responsive ui to not found page

--- frontend/src/pages/not-found-page.tsx
+++ frontend/src/pages/not-found-page.tsx
@@ -1,22 +1,26 @@
+import { AlertTriangleIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-8 py-12">
       <div className="flex flex-1 flex-col items-center justify-center gap-4">
-        <h1 className="text-8xl text-neutral-200">404</h1>
+        <div className="flex flex-col items-center">
+          <AlertTriangleIcon className="size-20 text-neutral-200 md:size-28" />
+          <h1 className="text-7xl text-neutral-200 md:text-8xl">404</h1>
+        </div>
 
         <div className="flex flex-col items-center gap-2">
-          <h1 className="text-damul-main-400 text-4xl font-bold">
+          <h1 className="text-damul-main-400 text-2xl font-bold md:text-4xl">
             존재하지 않는 페이지에요
           </h1>
-          <p className="text-lg text-neutral-500">URL을 확인해 주세요</p>
+          <p className="text-neutral-500 md:text-lg">URL을 확인해 주세요</p>
         </div>
       </div>
 
       <Link
         to={"/"}
-        className="text-damul-main-300 text-4xl font-extrabold select-none">
+        className="text-damul-main-300 text-2xl font-extrabold select-none md:text-4xl">
         DamulTalk
       </Link>
     </div>
