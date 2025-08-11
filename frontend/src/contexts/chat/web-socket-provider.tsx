17f7af2e1159b3e53a65b4e7b7aaf014e3129889
JongWoo <jinlaove17@naver.com>
2025-08-12 01:11:19 +0900

    [FE] fix: Modify sockjs constructor params

--- frontend/src/contexts/chat/web-socket-provider.tsx
+++ frontend/src/contexts/chat/web-socket-provider.tsx
@@ -49,7 +49,7 @@ const WebSocketProvider = ({ children }: WebSocketProviderProps) => {
       client = new Client({
         webSocketFactory: () =>
           new SockJS(
-            import.meta.env.VITE_WS_BASE_URL + `?token=${accessToken}`,
+            `${import.meta.env.VITE_WS_BASE_URL}?token=${accessToken}`,
             null,
             {
               transports: ["websocket"],
