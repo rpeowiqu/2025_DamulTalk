16f414f222552ab630292f0b880bff6ccae45691
JongWoo <jinlaove17@naver.com>
2025-08-17 22:47:32 +0900

    [FE] feat: Add dark mode to chat upload file item

--- frontend/src/components/chat/chat-upload-file-item.tsx
+++ frontend/src/components/chat/chat-upload-file-item.tsx
@@ -13,24 +13,24 @@ const ChatUploadFileItem = ({
   onDelete,
 }: ChatUploadFileItemProps) => {
   return (
-    <div className="relative flex h-16 w-64 items-center gap-2 rounded-xl border border-neutral-200 bg-white p-3">
+    <div className="relative flex h-16 w-full max-w-72 items-center gap-2 rounded-xl border border-neutral-200 bg-white p-3 dark:border-neutral-500 dark:bg-neutral-700">
       <img
         src={uploadFile.objectUrl}
         alt="업로드 파일 이미지"
-        className="size-12 object-cover"
+        className="size-10 object-cover"
       />
       <div className="flex flex-col gap-1 wrap-anywhere break-all">
         <h1 className="line-clamp-1 text-sm font-bold">
           {uploadFile.file.name}
         </h1>
-        <p className="line-clamp-1 text-xs text-neutral-500">
+        <p className="line-clamp-1 text-xs text-neutral-500 dark:text-neutral-200">
           {formatFileSize(uploadFile.file.size)}
         </p>
       </div>
 
       {onDelete && (
         <button
-          className="absolute top-1 right-1 cursor-pointer rounded-full bg-black/50 p-0.5 text-white transition-colors duration-200 hover:bg-black"
+          className="absolute top-1.5 right-1.5 cursor-pointer rounded-full bg-black/50 p-0.5 text-white transition-colors duration-200 hover:bg-black dark:bg-white/50 dark:text-black dark:hover:bg-white"
           onClick={onDelete}>
           <XIcon className="size-3" />
         </button>
