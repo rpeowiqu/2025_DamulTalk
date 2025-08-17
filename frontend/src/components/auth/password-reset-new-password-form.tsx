cbb688e92eb69eb339dcd8eafc16589e40eb3cef
JongWoo <jinlaove17@naver.com>
2025-08-17 21:59:05 +0900

    [FE] design: Modify message text size

--- frontend/src/components/auth/password-reset-new-password-form.tsx
+++ frontend/src/components/auth/password-reset-new-password-form.tsx
@@ -98,7 +98,7 @@ const PasswordResetNewPasswordForm = ({
 
           <p
             className={cn(
-              "text-sm",
+              "text-xs",
               messageType === "valid" ? "text-damul-main-300" : "text-red-400",
             )}>
             {message}
